1073c8ed5c8a136607fd4484cb520516d5ac1306
Ahmed <Ahmed@WAKEB>
2025-07-07 23:54:51 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,259 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data.Common;
+using System.Data.SQLite; // classes for database access
+// System.Data.SQLite is a.NET data provider for SQLite , which allows developers to interact with
+// SQLite databases using the ADO.NET programming model. This namespace will provide classes
+// and properties needed to access the SQLite database.
+
+// Another namespace you can use is the one provided by the Microsoft called
+// Microsoft.Data.Sqlite
+
+// Both libraries offer similar functionalities for working with SQLite databases, including
+// connection management, command execution, data retrieval, data manipulation, and
+// transaction support.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Start();
+        Console.WriteLine("Hello, World!");
+    }
+
+
+    static void Start()
+    {
+        Console.WriteLine("     ... it's a simple database have one tabel two colomn product and price ...");
+        using (SQLiteConnection sqliteConnection = CreateConnection())
+        {
+            CreateTable(sqliteConnection);
+            int choice;
+
+
+
+            do
+            {
+                Console.WriteLine(" ... Which Opreation You Want Execute ? ...");
+                Console.WriteLine(" 1. Create || 2. ReadAll || 3.Update || 4. Delete || 0. Exit");
+                Console.Write("* Enter Your Choice: ");
+
+                if (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Invalid input. Please enter a number from 0 to 4.");
+                    continue;
+                }
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("* Enter Product ID : ");
+                        if (int.TryParse(Console.ReadLine(), out int id));
+                        {
+                            Console.Write("* Enter Product Name : ");
+                            string name = Console.ReadLine();
+                            Console.Write("* Enter Product Price : ");
+                            if (int.TryParse(Console.ReadLine(), out int price))
+                                Create(sqliteConnection, id, name, price);
+                            
+                            //Console.Write("* Enter Product Quantity : ");
+                            //if (int.TryParse(Console.ReadLine(), out int quantity))
+                            //{
+                            //    Console.Write("* Enter Product Price : ");
+                            //    if (int.TryParse(Console.ReadLine(), out int price))
+                            //        CreateProduct(sqliteConnection, id, name!, quantity, price);
+                            //}
+                        }
+                       
+                        break;
+
+                    case 2:
+                        ReadAll(sqliteConnection);
+                        break;
+
+                    case 3:
+                        UpdateByName(sqliteConnection);
+                        break;
+
+                    case 4:
+                        DeleteByName(sqliteConnection);
+                        break;
+
+                    case 0:
+                        Console.WriteLine("Goodbye!");
+                        break;
+
+                    default:
+                        Console.WriteLine("Please choose a valid option from 0 to 4.");
+                        break;
+                }
+            } while (choice != 0);
+        }
+    }
+
+
+
+
+    // create connection with database
+    static SQLiteConnection CreateConnection()
+    {
+        SQLiteConnection sqliteConnection = new SQLiteConnection("Data Source=market.db; Version = 3; New = True; Compress = True");
+        try
+        {
+            if (sqliteConnection.State == System.Data.ConnectionState.Open)
+                return sqliteConnection;    
+            sqliteConnection.Open();
+        }
+        catch (Exception ex) 
+        {
+            Console.WriteLine($"Connection Error: {ex.Message}");
+        }
+        return sqliteConnection;
+    }
+
+
+
+    // create table or open it, if it exist
+    static void CreateTable(SQLiteConnection conn)
+    {
+        string SQLQueryCreateTabel = @"CREATE TABLE IF NOT EXISTS Product(Id INTEGER PRIMARY KEY,
+                                                                          Name VARCHAR(20),
+                                                                          Price INTEGER)";
+        using (SQLiteCommand command = conn.CreateCommand())
+        {
+            command.CommandText = SQLQueryCreateTabel;
+            command.ExecuteNonQuery();
+        }
+    }
+
+
+
+    // crud operation
+    // 1. Create
+    static void Create(SQLiteConnection connection,int id, string name, int price)
+    {
+        try
+        {
+                using (SQLiteCommand command = connection.CreateCommand())
+            {
+                //    With using	                    Without using
+                //  Automatically cleans up           You must manually call .Dispose()
+                //  Safer, less error-prone	          Risk of memory or resource leaks
+                //  Preferred in 99% of cases	      Only skip in special scenarios
+                command.CommandText = "INSERT INTO Product(Id, Name, Price) VALUES (@id, @name, @price)";
+                //command.CommandText = "INSERT INTO Product(Id, Name, Quantity, Price) VALUES (@id, @name, @quantity, @price)";
+                command.Parameters.AddWithValue("@id", id);
+                command.Parameters.AddWithValue("@name", name);
+                //command.Parameters.AddWithValue("@quantity", quantity);
+                command.Parameters.AddWithValue("@price", price);
+                command.ExecuteNonQuery();
+            }
+            Console.WriteLine($"Data Inserted Successfully: the product is {name} : {price} LE for unit");
+        }
+        catch (SQLiteException ex)
+        {
+            Console.WriteLine($"Insert Data Error : {ex.Message}");
+        }
+
+    }
+
+
+
+    // 2. read all table rows
+    static void ReadAll(SQLiteConnection connection)
+    {
+        using (SQLiteCommand command = connection.CreateCommand())
+        {
+            command.CommandText = "SELECT Id, Name, Price FROM Product";
+            using (SQLiteDataReader reader = command.ExecuteReader())
+            {
+                Console.WriteLine("\n📦 All Products:");
+                Console.WriteLine("{id} - {name} - {price}");
+                //Console.WriteLine("{id} - {name} - {quantity} - {price}");
+                while(reader.Read())
+                {
+                    int id = reader.GetInt32(0);
+                    string name = reader.GetString(1);
+                    //int quantity = reader.GetInt32(2);
+                    int price = reader.GetInt32(2);
+                    Console.WriteLine($" {id}   - {name}  - {price}");
+                }
+            }
+        }
+    }
+
+
+
+    // 3. update product
+    static void UpdateByName(SQLiteConnection connection)
+    {
+        Console.Write("Enter the Product Name to Update : ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter the New Price : ");
+        if (!int.TryParse(Console.ReadLine(), out int newPrice))
+        {
+            Console.WriteLine("Invalid price.");
+            return;
+        }
+        try
+        {
+            using (SQLiteCommand command = connection.CreateCommand())
+            {
+                command.CommandText = "UPDATE Product SET Price = @newPrice WHERE NAME = @name";
+                command.Parameters.AddWithValue("@name", name);
+                command.Parameters.AddWithValue("@newPrice", newPrice);
+                int rowsAffected = command.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                    Console.WriteLine($" Product '{name}' updated to new price: {newPrice}");
+                else
+                    Console.WriteLine($" No product found with name '{name}'.");
+            }
+        }
+        catch (SQLiteException ex)
+        {
+            Console.WriteLine($"Update Error: {ex.Message}");
+        }
+    }
+
+
+
+    // 4. delete product
+    static void DeleteByName(SQLiteConnection connection)
+    {
+        Console.Write("Enter the Product Name to delete: ");
+        string name = Console.ReadLine();
+
+        try
+        {
+            using (SQLiteCommand command = connection.CreateCommand())
+            {
+                command.CommandText = "DELETE FROM Product WHERE NAME = @name";
+                command.Parameters.AddWithValue("@name", name);
+                int rowsDeleted = command.ExecuteNonQuery();
+
+                if (rowsDeleted > 0)
+                    Console.WriteLine($" Product '{name}' deleted successfully.");
+                else
+                    Console.WriteLine($" No product found with name '{name}'.");
+            }
+        }
+        catch (SQLiteException ex)
+        {
+            Console.WriteLine($"Delete Error: {ex.Message}");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // 3. read single product
+}
